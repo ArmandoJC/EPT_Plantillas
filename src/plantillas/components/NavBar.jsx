37f48f32abf9e01b8f5a2fb97e7590f2ac68cc2e
ajaimes <armando.jaimes@ferreyros.com.pe>
2024-05-29 16:31:05 -0500

    Añadir buscador

--- src/plantillas/components/NavBar.jsx
+++ src/plantillas/components/NavBar.jsx
@@ -1,34 +1,85 @@
-import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material';
-import { LogoutOutlined, MenuOutlined } from '@mui/icons-material';
+import { AppBar, Grid, IconButton, Toolbar, Typography, InputBase } from '@mui/material';
+import { LogoutOutlined, MenuOutlined, Search as SearchIcon } from '@mui/icons-material';
 
+import { styled, alpha } from '@mui/material/styles';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '24ch',
+            '&:focus': {
+                width: '40ch',
+            },
+        },
+    },
+}));
 
 export const NavBar = ({ drawerWidth = 240 }) => {
-  return (
-    <AppBar 
-        position='fixed'
-        sx={{ 
-            width: { sm: `calc(100% - ${ drawerWidth }px)` },
-            ml: { sm: `${ drawerWidth }px` }
-         }}
-    >
-        <Toolbar>
-            <IconButton
-                color='inherit'
-                edge="start"
-                sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-                <MenuOutlined />
-            </IconButton>
+    return (
+        <AppBar
+            position='fixed'
+            sx={{
+                width: { sm: `calc(100% - ${drawerWidth}px)` },
+                ml: { sm: `${drawerWidth}px` }
+            }}
+        >
+            <Toolbar>
+                <IconButton
+                    color='inherit'
+                    edge="start"
+                    sx={{ mr: 2, display: { sm: 'none' } }}
+                >
+                    <MenuOutlined />
+                </IconButton>
 
-            <Grid container direction='row' justifyContent='space-between' alignItems='center'>
-                <Typography variant='h6' noWrap component='div'> Platillas App </Typography>
-
+                <Grid container direction='row' justifyContent='space-between' alignItems='center'>
+                    <Typography variant='h6' noWrap component='div'> Platillas App </Typography>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                </Grid>
                 <IconButton color='error'>
                     <LogoutOutlined />
                 </IconButton>
-            </Grid>
 
-        </Toolbar>
-    </AppBar>
-  )
+            </Toolbar>
+        </AppBar>
+    )
 }
